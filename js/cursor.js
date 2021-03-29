fc9efc0f2f883a32290d7c27e5c13b94f5502591
Instelce <intelce.developpement@gmail.com>
2021-03-29 11:04:22 +0200

    Add carousel.js

--- js/cursor.js
+++ js/cursor.js
@@ -3,13 +3,18 @@ const updateProperties = (elem, state) => {
     elem.style.setProperty('--y', `${state.y}px`)
     elem.style.setProperty('--width', `${state.width}px`)
     elem.style.setProperty('--height', `${state.height}px`)
+    elem.style.setProperty('--stroke', `${state.stroke}px`)
     elem.style.setProperty('--radius', state.radius)
     elem.style.setProperty('--scale', state.scale)
-    elem.style.setProperty('--fill', `rgba(242, 204, 143, 1)`)
+    elem.style.setProperty('--opacity', state.opacity)
+    elem.style.setProperty('--fill', state.fill)
+    elem.style.setProperty('--stroke-color', state.strokeColor)
 }
   
 document.querySelectorAll('.cursor').forEach(cursor => {
-    let onElement;
+    let elemHoverTopbarToggle;
+    let elemHoverCarousel;
+    let elemHoverTextLink;
   
     const createState = e => {
         const defaultState = {
@@ -17,20 +22,50 @@ document.querySelectorAll('.cursor').forEach(cursor => {
             y: e.clientY,
             width: 40,
             height: 40,
-            radius: '50%'
+            scale: '1',
+            stroke: 1,
+            radius: '50%',
+            opacity: '1',
+            fill: `rgba(242, 204, 143, 0)`,
+            strokeColor: `rgba(242, 204, 143, 1)`,
         }
     
         const computedState = {}
     
-        if (onElement != null) {
-            const { top, left, width, height } = onElement.getBoundingClientRect()
-            const radius = window.getComputedStyle(onElement).borderTopLeftRadius
+        if (elemHoverTopbarToggle != null) {
+            const { top, left, width, height } = elemHoverTopbarToggle.getBoundingClientRect()
+            const radius = window.getComputedStyle(elemHoverTopbarToggle).borderTopLeftRadius
 
             computedState.x = left + width / 2
             computedState.y = top + height / 2
             computedState.width = width
             computedState.height = height
             computedState.radius = radius
+            computedState.opacity = `0.8`
+            computedState.fill = `rgba(242, 204, 143, 1)`
+        }
+
+        if (elemHoverCarousel != null) {
+            const { top, left, width, height } = elemHoverCarousel.getBoundingClientRect()
+            const radius = window.getComputedStyle(elemHoverCarousel).borderTopLeftRadius
+
+            computedState.x = left + width / 2
+            computedState.y = top + height / 2
+            computedState.width = width
+            computedState.height = height
+            computedState.stroke = 1;
+            computedState.radius = radius
+            computedState.opacity = `0.8`
+            // computedState.strokeColor = `rgb(244, 226, 133)`
+        }
+
+        if (elemHoverTextLink != null) {
+            // const { top, left, width, height } = elemHoverTextLink.getBoundingClientRect()
+
+            computedState.stroke = 1;
+            computedState.opacity = `0.6`
+            computedState.fill = `rgba(242, 204, 143, 1)`
+            // computedState.strokeColor = `rgb(244, 226, 133)`
         }
   
         return {
@@ -44,8 +79,18 @@ document.querySelectorAll('.cursor').forEach(cursor => {
         updateProperties(cursor, state)
     })
   
-    document.querySelectorAll('.cursor-hover-1').forEach(elem => {
-        elem.addEventListener('mouseenter', () => (onElement = elem))
-        elem.addEventListener('mouseleave', () => (onElement = undefined))
+    document.querySelectorAll('.cursor__hover-1').forEach(elem => {
+        elem.addEventListener('mouseenter', () => (elemHoverTopbarToggle = elem))
+        elem.addEventListener('mouseleave', () => (elemHoverTopbarToggle = undefined))
+    })
+
+    document.querySelectorAll('.cursor__hover-2').forEach(elem => {
+        elem.addEventListener('mouseenter', () => (elemHoverCarousel = elem))
+        elem.addEventListener('mouseleave', () => (elemHoverCarousel = undefined))
+    })
+
+    document.querySelectorAll('.cursor__hover-text').forEach(elem => {
+        elem.addEventListener('mouseenter', () => (elemHoverTextLink = elem))
+        elem.addEventListener('mouseleave', () => (elemHoverTextLink = undefined))
     })
 })
--- js/main.js
+++ js/main.js
@@ -1,31 +1,27 @@
-// import Cursor from "./cursor";
+// // Topbar Animation Hover Link
+// const topbar = document.querySelector('.topbar')
+// var firstspan = document.querySelector('.topbar__name span:nth-child(1)');
+// firstspan.innerHTML = firstspan.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-// const cursor = new Cursor();
-
-
-// Topbar Animation Hover Link
-const topbar = document.querySelector('.topbar')
-var firstspan = document.querySelector('.topbar__name span:nth-child(1)');
-firstspan.innerHTML = firstspan.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-topbar.addEventListener('mouseover', function(){
-    anime.timeline({loop: false})
-    .add({
-      targets: '.topbar__name span .letter',
-      translateY: [-50,0],
-      easing: "easeOutExpo",
-      duration: 100,
-      delay: (el, i) => 30 * i
-    })
-});
+// topbar.addEventListener('mouseover', function(){
+//     anime.timeline({loop: false})
+//     .add({
+//       targets: '.topbar__name span .letter',
+//       translateY: [-50,0],
+//       easing: "easeOutExpo",
+//       duration: 100,
+//       delay: (el, i) => 30 * i
+//     })
+// });
 
 // Header Parralax
-document.addEventListener('mousemove', parallax);
-function parallax(e) {
-  this.querySelectorAll('.header__layer').forEach(layer => {
-    var speed = layer.getAttribute('data-speed');
-    var x = (window.innerWidth - e.pageX * speed) / 100;
-    var y = (window.innerWidth - e.pageY * speed) / 100;
-    layer.style.transform = `translate(${x}px, ${y}px)`
-  });
-}
+// document.addEventListener('mousemove', parallax);
+
+// function parallax(e) {
+//   this.querySelectorAll('.header__layer').forEach(layer => {
+//     var speed = layer.getAttribute('data-speed');
+//     var x = (window.innerWidth - e.pageX * speed) / 100;
+//     var y = (window.innerWidth - e.pageY * speed) / 100;
+//     layer.style.transform = `translate(${x}px, ${y}px)`
+//   });
+// }
